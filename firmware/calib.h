7fe0b08f65ab312f6cf093003f5d9ac5c2d9c962
Petre Rodan <petre.rodan@simplex.ro>
2013-11-16 16:58:57 +0200

    first commit

---
+++ firmware/calib.h
@@ -0,0 +1,9 @@
+#ifndef __CALIB_H__
+#define __CALIB_H__
+
+// see temperature conversion equation in micronix_pyrometer_conversion.ods (OpenOffice spreadsheet)
+
+#define PYR_A         -17.19
+#define PYR_B         0.0039
+
+#endif
